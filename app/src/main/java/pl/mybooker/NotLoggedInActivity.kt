8ba0248a4ecf172213388aec0cf82c3c94c89852
Mike <certexplay@gmail.com>
2019-04-14 16:32:27 +0200

    added register and notloggedin activities

---
+++ app/src/main/java/pl/mybooker/NotLoggedInActivity.kt
@@ -0,0 +1,12 @@
+package pl.mybooker
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class NotLoggedInActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_not_logged_in)
+  }
+}
